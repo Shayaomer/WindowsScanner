a89b6dee239135e23e97d7de664c66ed4db5fdc2
Shaya Omer <os15587@gmail.com>
2022-01-11 13:47:32 +0200

    minor changes

--- matching_cve_cpe.py
+++ matching_cve_cpe.py
@@ -15,24 +15,21 @@ class MatcherCveCpe:
         print('Fitting results...')
         cpe_sw_fitter = sEngine.CpeSwFitter("parsed_xml.csv", "cosin")
         self.cpe_data_dict = cpe_sw_fitter.fit_all(1)
-        print('Engine finished and CPE results dumped!')
+        print('Engine finished and CPE-Installed softwares results dumped!')
 
     def match_cve_cpe(self):
-        print('Matching CVE-CPE...')
         # Initialize data frame items
         sftw_names = list(self.cpe_data_dict['registry_sw'].values())
         cpe_23_names = list(self.cpe_data_dict['cpe_23_names'].values())
         sim_score = list(self.cpe_data_dict['sim_score'].values())
         asso_cve = []
-        for i in range(len(sim_score)):
-            asso_cve.append([])
         df = pd.DataFrame([sftw_names, cpe_23_names, sim_score, asso_cve]).transpose()
         df.columns = ['sftw_name', 'cpe_23', 'sim_score', 'asso_cve']
 
         # Matching process
         cve_gen = self.cve_funcs.get_all_cpe23_uri()
         _dict = {}
-        for cpe_23, cve_id in tqdm(cve_gen, desc="Matching"):
+        for cpe_23, cve_id in tqdm(cve_gen, desc="Matching CPE-CVE"):
             _dict[cpe_23] = _dict.get(cpe_23, []) + [cve_id]
         df['asso_cve'] = df['cpe_23'].apply(lambda x: _dict[x] if x in _dict else [])
         df.to_csv('result.csv')
