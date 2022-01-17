a58d11518a928dda843eb80101cef603dbac1354
Shaya Omer <os15587@gmail.com>
2022-01-18 01:07:48 +0200

    added option to run through main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from db import collection
 from installed_softwares import InstalledSoftware
 from xmlParser import CpeXmlParser
 from matching_cve_cpe import MatcherCveCpe
@@ -9,7 +10,7 @@ import socket
 def execute():
     print('Initializing the scan & matching process...')
     print("Downloading CVE data...")
-    a = DownloadDb()
+    DownloadDb()
     print("Downloading CPE data...")
     download_db.download_file()
     download_db.unzip_file('official-cpe-dictionary_v2.3.xml.zip', directory_to_extract=None)
@@ -26,3 +27,14 @@ def execute():
     res_json = c.match_cve_cpe()
     return {'_id': socket.gethostname(),
             '_data': res_json}
+
+# Run this to scan & upload to the mongoDB cloud database
+# Scan result will also be saved at json_final_res.json
+if __name__ == '__main__':
+    c = execute()
+    comp_name = str(socket.gethostname())
+    if collection.find_one({'_id': comp_name}) == None:
+        collection.insert_one(c)
+    else:
+        collection.delete_one({'_id': comp_name})
+        collection.insert_one(c)
